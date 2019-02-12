d15dc3040cd54aa7336243aa69952d0e13530484
dapperauteur <a@awews.com>
2019-02-11 22:46:54 -0700

    add higher/lower buttons and logic, heading and guess text

--- number_wizard_ui/NumberWizardUI/Assets/Scripts/SceneLoader.cs
+++ number_wizard_ui/NumberWizardUI/Assets/Scripts/SceneLoader.cs
@@ -5,6 +5,10 @@ using UnityEngine.SceneManagement;
 
 public class SceneLoader : MonoBehaviour
 {
+    int max;
+    int min;
+    int guess;
+
     public void LoadNextScene()
     {
         int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
@@ -14,5 +18,44 @@ public class SceneLoader : MonoBehaviour
     public void LoadStartScene()
     {
         SceneManager.LoadScene(0);
+    }
+
+    public void StartGame()
+    {
+        max = 1000;
+        min = 1;
+        guess = 500;
+    }
+
+    void Start()
+    {
+        Debug.Log("start: " + max + " " + min + " " + guess);
+        StartGame();
+        Debug.Log(max + " " + min + " " + guess);
+    }
+
+    /*
+    private void Update()
+    {
+        Debug.Log("update");
+    }
+    */
+
+    public void ItsHigher()
+    {
+        Debug.Log("its higher: " + max + " " + min + " " + guess);
+        min = guess;
+        Debug.Log(max + " " + min + " " + guess);
+        guess = (max + min) / 2;
+        Debug.Log(max + " " + min + " " + guess);
+    }
+
+    public void ItsLower()
+    {
+        Debug.Log("its lower: " + max + " " + min + " " + guess);
+        max = guess;
+        Debug.Log(max + " " + min + " " + guess);
+        guess = (max + min) / 2;
+        Debug.Log(max + " " + min + " " + guess);
     }
 }
