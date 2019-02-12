5535f78795fb4a02cb1297a592b6232571791d6c
dapperauteur <a@awews.com>
2019-02-11 19:12:45 -0700

    complete section 3

---
+++ Name Wizard Console/Assets/NumberWizard.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NumberWizard : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Welcome to Number Wizard");
+        Debug.Log("Please pick a number");
+        Debug.Log("The highest number can be 100");
+        Debug.Log("The lowest the number can be is 0");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
