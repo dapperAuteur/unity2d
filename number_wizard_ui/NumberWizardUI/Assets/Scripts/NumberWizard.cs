b7bd9f4b5e9f381daa1cd08da4d23ae9a86fa421
dapperauteur <a@awews.com>
2019-02-15 18:46:56 -0700

    finish lecture 42 Use Random.Range(), still getting 'NullReferenceException

--- number_wizard_ui/NumberWizardUI/Assets/Scripts/NumberWizard.cs
+++ number_wizard_ui/NumberWizardUI/Assets/Scripts/NumberWizard.cs
@@ -19,26 +19,28 @@ public class NumberWizard : MonoBehaviour
 
     void StartGame()
     {
-        guess = (max + min) / 2;
-        guessText.text = guess.ToString();
-        max = max + 1;
+        NextGuess();
+        //max = max + 1;
     }
 
     public void OnPressHigher()
     {
-        min = guess;
+        //min = guess;
+        min = guess + 1;
         NextGuess();
     }
 
     public void OnPressLower()
     {
-        max = guess;
+        //max = guess;
+        max = guess - 1;
         NextGuess();
     }
 
     void NextGuess()
     {
-        guess = (max + min) / 2;
+        //guess = (max + min) / 2;
+        guess = Random.Range(min, max + 1);
         Debug.Log("Is it higher or lower than..." + guess);
         guessText.text = guess.ToString();
     }
